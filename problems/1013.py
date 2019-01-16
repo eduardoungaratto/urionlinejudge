0820d5bc863645c0f968dfedf62e14c239020444
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-01-16 00:18:06 -0200

    bug fix, problem solved

--- problems/1013.py
+++ problems/1013.py
@@ -5,8 +5,25 @@ s = s.split(" ")
 a = int(s[0])
 b = int(s[1])
 c = int(s[2])
-x = int((a+b+abs(a-b))/2)
-x = int((b+c+abs(b-c))/2)
-x = int((a+c+abs(a-c))/2)
+y = 0
 
-print(str(x)+" eh o maior")
+x = int((a+b+abs(a-b))/2)
+if x > y: 
+	y = x
+
+
+x = int((b+a+abs(b-a))/2)
+if x > y: 
+	y = x
+
+
+x = int((a+c+abs(a-c))/2)
+if x > y: 
+	y = x
+
+
+x = int((b+c+abs(b-c))/2)
+if x > y: 
+	y = x
+
+print(str(y)+" eh o maior")
