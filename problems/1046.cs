3266507174c1a2eb58c72d2dce2d7ae2be1fba19
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-01-15 23:25:10 -0200

    new solve problem

---
+++ problems/1046.cs
@@ -0,0 +1,27 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+		String[] vetHoras = Console.ReadLine().Split(" ");
+		int horaInicial = int.Parse(vetHoras[0]);
+		int horaFinal = int.Parse(vetHoras[1]);
+		
+		if (horaInicial > horaFinal)
+		{
+			int tempoDuracao = (24 - ( ( (horaInicial - horaFinal) ) ) );
+			Console.WriteLine("O JOGO DUROU " + tempoDuracao * (1) + " HORAS");
+		} else if (horaInicial < horaFinal)
+		{
+			int tempoDuracao = (horaFinal - horaInicial);
+			Console.WriteLine("O JOGO DUROU " + tempoDuracao * (1) + " HORAS");
+		} else if ( horaInicial == horaFinal)
+		{
+			int tempoDuracao = ((horaInicial - horaFinal)-24);
+			Console.WriteLine("O JOGO DUROU " + tempoDuracao * (-1) + " HORAS");
+		}
+
+    }
+
+}
