6afaf91a31b665966b95b1e4e410efafca113d3b
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-01-29 22:51:17 -0200

    new solved problems and modified other

--- problems/1013.py
+++ problems/1013.py
@@ -5,25 +5,25 @@ s = s.split(" ")
 a = int(s[0])
 b = int(s[1])
 c = int(s[2])
-y = 0
+maior = 0
 
 x = int((a+b+abs(a-b))/2)
 if x > y: 
-	y = x
+	maior = x
 
 
 x = int((b+a+abs(b-a))/2)
 if x > y: 
-	y = x
+	maior = x
 
 
 x = int((a+c+abs(a-c))/2)
 if x > y: 
-	y = x
+	maior = x
 
 
 x = int((b+c+abs(b-c))/2)
 if x > y: 
-	y = x
+	maior = x
 
-print(str(y)+" eh o maior")
+print(str(maior)+" eh o maior")
