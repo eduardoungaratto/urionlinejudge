48fc6f37d7de6bb6e9e9a02650cf6457447cad5b
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-02-14 22:17:51 -0200

    solving problem process

---
+++ problems/1018.py
@@ -0,0 +1,58 @@
+valor = input ("Valor:")
+print("")
+print("Nota de R$:100,00")
+valora = int(valor) % 100
+valorb = int(valor) / 100
+valorb = int(valorb)
+print("Div = Notas = ",valorb)
+#print("Mod = ",valora)
+print("")
+print("Nota de R$:50,00")
+valorc = valora % 50
+valord = valora /50
+valord = int(valord)
+print("Div = Notas = ", valord)
+#print("Mod = ", valorc)
+print("")
+print("Nota de R$:20,00")
+valore = valorc % 20
+valorf = valorc / 20
+valorf = int(valorf)
+print("Div = Notas = ", valorf)
+#print("Mod = ", valore)
+print("")
+print("Nota de R$:10,00")
+valorg = valore % 10
+valorh = valore / 10
+valorh = int(valorh)
+print("Div = Notas = ",valorh)
+#print("Mod = ",valorg)
+print("")
+print("Nota de R$:5,00")
+valori = valorg % 5
+valorj = valorg / 5
+valorj = int(valorj)
+print("Div = Notas = ", valorj)
+#print("Mod", valori)
+print("")
+print("Nota de R$:2,00")
+valork = valori % 2
+valorl = valori / 2
+valorl = int(valorl)
+print("Div = Notas = ", valorl)
+#print("Mod", valork)
+print("")
+print("Nota de R$:1,00")
+valorm = valork % 1
+valorn = valork / 1
+valorn = int(valorn)
+print("Div = Notas = ", valorn)
+#print("Mod", valorm)
+print("")
+
+
+
+
+
+
+
