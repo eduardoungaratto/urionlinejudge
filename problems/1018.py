a7ce041c9d954d0d8e53e1a512c46df8ff764bab
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-05-28 20:14:14 -0300

    problem solved

--- problems/1018.py
+++ problems/1018.py
@@ -1,58 +1,38 @@
-valor = input ("Valor:")
-print("")
-print("Nota de R$:100,00")
+valor = input()
+
+print(valor)
+
 valora = int(valor) % 100
 valorb = int(valor) / 100
 valorb = int(valorb)
-print("Div = Notas = ",valorb)
-#print("Mod = ",valora)
-print("")
-print("Nota de R$:50,00")
+print(valorb , "nota(s) de R$ 100,00")
+
 valorc = valora % 50
-valord = valora /50
+valord = valora / 50
 valord = int(valord)
-print("Div = Notas = ", valord)
-#print("Mod = ", valorc)
-print("")
-print("Nota de R$:20,00")
+print(valord , "nota(s) de R$ 50,00")
+
 valore = valorc % 20
 valorf = valorc / 20
 valorf = int(valorf)
-print("Div = Notas = ", valorf)
-#print("Mod = ", valore)
-print("")
-print("Nota de R$:10,00")
+print(valorf , "nota(s) de R$ 20,00")
+
 valorg = valore % 10
 valorh = valore / 10
 valorh = int(valorh)
-print("Div = Notas = ",valorh)
-#print("Mod = ",valorg)
-print("")
-print("Nota de R$:5,00")
+print(valorh , "nota(s) de R$ 10,00")
+
 valori = valorg % 5
 valorj = valorg / 5
 valorj = int(valorj)
-print("Div = Notas = ", valorj)
-#print("Mod", valori)
-print("")
-print("Nota de R$:2,00")
+print(valorj , "nota(s) de R$ 5,00")
+
 valork = valori % 2
 valorl = valori / 2
 valorl = int(valorl)
-print("Div = Notas = ", valorl)
-#print("Mod", valork)
-print("")
-print("Nota de R$:1,00")
+print(valorl , "nota(s) de R$ 2,00")
+
 valorm = valork % 1
 valorn = valork / 1
 valorn = int(valorn)
-print("Div = Notas = ", valorn)
-#print("Mod", valorm)
-print("")
-
-
-
-
-
-
-
+print(valorn , "nota(s) de R$ 1,00")
