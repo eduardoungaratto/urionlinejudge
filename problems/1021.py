46dd0b93045bc77aa337124ea8a6193381e97ea4
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-05-31 01:44:14 +0000

    1021 solving

--- problems/1021.py
+++ problems/1021.py
@@ -32,34 +32,26 @@ print(int(valorn) , "nota(s) de R$ 1,00")
 
 #0.50
 valordv050 = valorm / 0.50
-print ('moedas de 050 ' +  str(int(valordv050)));
+print (str(int(valordv050)) + 'moeda(s) de R$ 0.50');
 valordf050 = valorm % 0.50
-
-print("------------------------------")
 
 #0.25
 valordv025 = valordf050 / 0.25
-print("moedas de 025 " + str(int(valordv025)));
+print(str(int(valordv025)) + "moeda(s) de R$ 0.25 ");
 valordf025 = valordf050 % 0.25
-
-print("------------------------------")
 
 #0.10
 valordv010 = valordf025 / 0.10
-print("moedas de 010 " + str(int(valordv010)));
+print(str(int(valordv010)) + "moeda(s) de R$ 0.10 ");
 valordf010 = valordf025 % 0.10
-
-print("------------------------------")
 
 #0.05
 valordf010
 valordv005 = valordf010 / 0.05
-print("moedas de 005 " + str(int(valordv005)));
+print(str(int(valordv005)) + "moeda(s) de R$ 0.05");
 valordf005 = valordf010 % 0.05
-
-print("------------------------------")
 
 #0.01
 valordv001 = valordf005 / 0.01
-print("moedas de 001 " + str(int(valordv001)));
+print(str(int(valordv001)) + "moeda(s) de R$ 0.01");
 #valordf001 = valordf005 % 0.01
