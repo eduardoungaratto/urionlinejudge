6c6b8b26d900620a520eaade9cdd9782f56d3672
Eduardo Ungaratto <eduardo.ungarattolima@gmail.com>
2019-06-12 11:48:23 -0300

    delete
    wrong commit location

--- 1035.py
+++
@@ -1,8 +0,0 @@
-ipts = raw_input(str())
-ipts = ipts.split(' ')
-
-//iteract over each position and convert the element to int
-idx = 0
-for ipt in ipts:
- ipts[idx] = int(ipts[idx])
- idx = idx + 1
