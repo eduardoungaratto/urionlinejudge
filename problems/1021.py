21bd07a5c902a4481e47007b673893da53bcca9f
eduardoungaratto <eduardo.ungarattolima@gmail.com>
2019-06-21 15:50:24 -0300

    files

--- problems/1021.py
+++ problems/1021.py
@@ -1,58 +1,60 @@
+import os
+
 valor = float('{:0.3f}'.format(float(input()))) #TODO
 
-print("NOTAS:");
+os.system('cls')
 
+print("NOTAS:");
 valora = valor % 100
 valorb = valor / 100
-print("nota(s) de R$ 100,00" + str(int(valorb)));
+print(str(int(valorb)) + " nota(s) de R$ 100.00");
 
 valorc = valora % 50
 valord = valora / 50
-print("nota(s) de R$ 50,00" + str(int(valord)));
+print(str(int(valord)) + " nota(s) de R$ 50.00");
 
 valore = valorc % 20
 valorf = valorc / 20
-print("nota(s) de R$ 20,00" + str(int(valorf)));
+print(str(int(valorf)) + " nota(s) de R$ 20.00");
 
 valorg = valore % 10
 valorh = valore / 10
-print("nota(s) de R$ 10,00" + str(int(valorh)));
+print(str(int(valorh)) + " nota(s) de R$ 10.00");
 
 valori = valorg % 5
 valorj = valorg / 5
-print("nota(s) de R$ 5,00" + str(int(valorj)));
+print(str(int(valorj)) + " nota(s) de R$ 5.00");
 
 valork = valori % 2
 valorl = valori / 2
-print("nota(s) de R$ 2,00" + str(int(valorl)));
+print(str(int(valorl)) + " nota(s) de R$ 2.00");
 
 print("MOEDAS:");
-
 valorm = valork % 1
 valorn = valork / 1
-print("moeda(s) de R$ 1,00" + str(int(valorn)));
+print(str(int(valorn)) + " moeda(s) de R$ 1.00");
 
 #0.50
 valordv050 = valorm / 0.50
-print ('moeda(s) de R$ 0.50 ' +  str(int(valordv050)));
+print (str(int(valordv050)) + " moeda(s) de R$ 0.50");
 valordf050 = valorm % 0.50
 
 #0.25
 valordv025 = valordf050 / 0.25
-print("moeda(s) de R$ 0.25 " + str(int(valordv025)));
+print(str(int(valordv025)) + " moeda(s) de R$ 0.25");
 valordf025 = valordf050 % 0.25
 
 #0.10
 valordv010 = valordf025 / 0.10
-print("moeda(s) de R$ 0.10 " + str(int(valordv010)));
+print(str(int(valordv010)) + " moeda(s) de R$ 0.10");
 valordf010 = valordf025 % 0.10
 
 #0.05
 valordv005 = valordf010 / 0.05
-print("moeda(s) de R$ 0.05 " + str(int(valordv005)));
+print(str(int(valordv005)) + " moeda(s) de R$ 0.05");
 valordf005 = valordf010 % 0.05
 
 #0.01
 valordv001 = valordf005 / 0.01
-print(str(int(valordv001)) + "moeda(s) de R$ 0.01");
+print(str(int(valordv001)) + " moeda(s) de R$ 0.01");
 #valordf001 = valordf005 % 0.01
